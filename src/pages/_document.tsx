2b0cccbd187f2af52072b7d515d4de61bd0511c9
Raniere Marques <Raniere852@outlook.com>
2022-06-14 15:46:52 -0300

    fix: change css order

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,12 +5,12 @@ export default function Document() {
   return (
     <Html lang="pt-BR">
       <Head>
-        <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
         <style
           dangerouslySetInnerHTML={{
             __html: `@font-face {font-family: 'IBM Plex Sans';font-style: normal;font-weight: 100 700;font-display: fallback;src: url(/fonts/ibm-plex-sans-var.woff2) format('woff2');}@font-face {font-family: 'Jet Brains Mono';font-style: normal;font-weight: 100 800;font-display: fallback;src: url('/fonts/jet-brains-mono-var.woff2') format('woff2');}`,
           }}
         />
+        <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
       </Head>
       <body>
         <Main />
