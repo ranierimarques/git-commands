b9b60878404d286a964213d2182c0fa7a82222b0
Raniere Marques <Raniere852@outlook.com>
2022-06-13 21:58:03 -0300

    feat: add favicons and reset project

---
+++ src/pages/_app.tsx
@@ -0,0 +1,35 @@
+import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import '../styles/globals.css'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#da532c" />
+        <meta name="msapplication-TileColor" content="#da532c" />
+        <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
+        <meta name="theme-color" content="#da532c" />
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default MyApp
