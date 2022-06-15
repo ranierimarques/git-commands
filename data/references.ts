55b0815645d75a9c3b1b0847e005e64044079ceb
Raniere Marques <Raniere852@outlook.com>
2022-06-15 06:43:32 -0300

    refactor: text improvemens

--- data/references.ts
+++ data/references.ts
@@ -7,8 +7,7 @@ export const references = [
         code: 'git config --list',
       },
       {
-        title:
-          'Vincular uma palavra ou letra à um comando existente. Agora é possível utilizar git s para executar git status.',
+        title: 'Vincular uma palavra ou letra à um comando existente.',
         code: 'git config --global alias.s status',
       },
     ],
@@ -22,7 +21,7 @@ export const references = [
       },
       {
         title: 'Clonar um repositório online.',
-        code: 'git clone [URL | SSH] [Nome do repositório] (Omitir para salvar com o nome do repositório atual)',
+        code: 'git clone [URL | SSH] [NOME DO REPOSITÓRIO]',
       },
     ],
   },
@@ -30,7 +29,7 @@ export const references = [
     title: 'Versionamento básico',
     commands: [
       {
-        title: 'Preparar para commit todas as mudanças.',
+        title: 'Preparar todas as mudanças para commit.',
         code: 'git add .',
       },
       {
@@ -38,7 +37,7 @@ export const references = [
         code: 'git status',
       },
       {
-        title: 'Mostrar as modificações na Working Tree atual.',
+        title: 'Mostrar as modificações na Working Tree.',
         code: 'git diff',
       },
       {
@@ -47,11 +46,11 @@ export const references = [
       },
       {
         title: 'Criar um commit com uma mensagem.',
-        code: 'git commit -m "mensagem"',
+        code: 'git commit -m "Mensagem"',
       },
       {
-        title: 'Mover um arquivo de Staged para Modified/Untrack',
-        code: 'git reset HEAD [Nome do arquivo]',
+        title: 'Mover um arquivo de Staged para Modified/Untrack.',
+        code: 'git reset HEAD [NOME DO ARQUIVO]',
       },
       {
         title:
@@ -65,7 +64,7 @@ export const references = [
       },
       {
         title:
-          'Voltar para o commit excluindo os anteriores e com os arquivos do commit excluídos (Cuidado).',
+          'Voltar para o commit excluindo os anteriores e excluindo os arquivos do commit (Cuidado).',
         code: 'git reset --hard [HASH]',
       },
     ],
@@ -79,32 +78,31 @@ export const references = [
       },
       {
         title: 'Criar uma branch.',
-        code: 'git branch [Nome da branch]',
+        code: 'git branch [NOME DA BRANCH]',
       },
       {
         title: 'Apagar uma branch localmente.',
-        code: 'git branch -d [Nome da branch]',
+        code: 'git branch -d [NOME DA BRANCH]',
       },
       {
         title: 'Apagar uma branch forçadamente.',
-        code: 'git branch -D [Nome da branch]',
+        code: 'git branch -D [NOME DA BRANCH]',
       },
       {
         title: 'Navegar até a branch.',
-        code: 'git checkout [Nome da branch]',
+        code: 'git checkout [NOME DA BRANCH]',
       },
       {
         title: 'Criar uma branch e navegar para ela.',
-        code: 'git checkout -b [Nome da branch]',
+        code: 'git checkout -b [NOME DA BRANCH]',
       },
       {
         title: 'Descartar as mudanças naquele arquivo.',
-        code: 'git checkout [Nome do arquivo]',
+        code: 'git checkout [NOME DO ARQUIVO]',
       },
       {
-        title:
-          'Juntar os dados de uma branch selecionada para a branch atual. Esse comando criará um commit extra para unir as branches.',
-        code: 'git merge [Branch]',
+        title: 'Juntar os dados de uma branch selecionada para a branch atual.',
+        code: 'git merge [NOME DA BRANCH]',
       },
       {
         title: 'Criar um Stash das mudanças.',
@@ -140,34 +138,32 @@ export const references = [
     title: 'Compartilhamento e atualização de projetos',
     commands: [
       {
-        title:
-          'Enviar os commits do repositório atual para um repositório remoto. Ex: git push [origin] [main]',
-        code: 'git push [Nome do repositório remoto] [BRANCH ATUAL]',
+        title: 'Enviar commits de uma branch para um repositório remoto.',
+        code: 'git push [NOME DO REPOSITÓRIO REMOTO] [NOME DA BRANCH]',
       },
       {
-        title: 'Enviar as tags do repositório atual para um repositório remoto',
-        code: 'git push [Nome do repositório remoto] [BRANCH ATUAL] --tags',
+        title: 'Enviar tags de uma branch para um repositório remoto',
+        code: 'git push [NOME DO REPOSITÓRIO REMOTO] [NOME DA BRANCH] --tags',
       },
-
       {
         title: 'Apagar tags e branches remotos',
-        code: 'git push [Nome do repositório remoto] :[Nome da Tag ou Nome do Branch]',
+        code: 'git push [NOME DO REPOSITÓRIO REMOTO] :[NOME DA TAG | NOME DA BRANCH]',
       },
       {
-        title: 'Listar o nome de todos os repositórios remotos.',
+        title: 'Listar todos os repositórios remotos.',
         code: 'git remote',
       },
       {
-        title: 'Criar um remote para um URL existente.',
-        code: 'git remote add [nome] [URL | SSH]',
-      },
-      {
-        title: 'Renomear um remote existente.',
-        code: 'git remote rename [atual] [novo]',
-      },
-      {
-        title: 'Listar o nome e o link de todos os repositórios remotos.',
+        title: 'Listar todos os repositórios remotos com mais detalhes.',
         code: 'git remote -v',
+      },
+      {
+        title: 'Conectar um repositório remoto.',
+        code: 'git remote add [NOME DO REPOSITÓRIO REMOTO] [URL | SSH]',
+      },
+      {
+        title: 'Renomear um repositório remoto.',
+        code: 'git remote rename [NOME DO REPOSITÓRIO REMOTO ATUAL] [NOME DO REPOSITÓRIO REMOTO NOVO]',
       },
     ],
   },
@@ -175,27 +171,28 @@ export const references = [
     title: 'Inspeção e Comparação',
     commands: [
       {
-        title: 'Visualizar as modificações feita em um Commit usando o Hash dele.',
+        title: 'Visualizar as modificações feita em um commit.',
         code: 'git show [HASH]',
       },
       {
-        title: 'Visualizar o histórico de Commits.',
+        title: 'Visualizar o histórico de commits.',
         code: 'git log',
       },
       {
-        title: 'Filtrar o histórico de Commits pelo Author especificado.',
-        code: 'git log --author="João"',
-      },
-      {
-        title: 'Visualizar em modo de gráfico o histórico de Commits.',
+        title: 'Visualizar o histórico de commits em gráfico.',
         code: 'git log --graph',
       },
       {
-        title: 'Visualizar o histórico de Commits resumido.',
+        title: 'Filtrar o histórico de commits pelo autor.',
+        code: 'git log --author="Nome do autor"',
+      },
+      {
+        title: 'Visualizar o histórico de commits resumido.',
         code: 'git shortlog',
       },
       {
-        title: 'Filtrar o histórico de Commits pela quantidade de Commits.',
+        title:
+          'Visualizar o histórico de commits pela quantidade de commits dos autores.',
         code: 'git shortlog -sn',
       },
     ],
@@ -204,13 +201,11 @@ export const references = [
     title: 'Aplicação de patches',
     commands: [
       {
-        title:
-          'Juntar os dados de uma branch selecionada para a branch atual. O commit irá para o topo da lista.',
-        code: 'git rebase [Branch]',
+        title: 'Juntar os dados de uma branch selecionada para a branch atual.',
+        code: 'git rebase [NOME DA BRANCH]',
       },
       {
-        title:
-          'Cria um novo commit sem as alterações do commit revertido e não apaga o commit revertido do histórico.',
+        title: 'Desfaz um commit sem remover o commit do histórico.',
         code: 'git revert [HASH]',
       },
     ],
