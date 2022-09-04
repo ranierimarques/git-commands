4aa44797de0578ac5283958cf43ebcdd19015984
Raniere Marques <Raniere852@outlook.com>
2022-09-04 08:36:14 -0300

    fix: add tooltip provider

--- src/components/tooltip/tooltip.tsx
+++ src/components/tooltip/tooltip.tsx
@@ -5,17 +5,19 @@ type tooltipProps = any
 
 export function Tooltip({ children, content, icon, ...props }: tooltipProps) {
   return (
-    <TooltipPrimitive.Root delayDuration={icon ? 0 : 200}>
-      <S.StyledTrigger>{children}</S.StyledTrigger>
-      <S.StyledContent
-        sideOffset={icon ? 8 : 14}
-        side={icon ? 'right' : 'top'}
-        align="center"
-        {...props}
-      >
-        {content}
-        <TooltipPrimitive.Arrow width={18} height={8} />
-      </S.StyledContent>
-    </TooltipPrimitive.Root>
+    <TooltipPrimitive.Provider>
+      <TooltipPrimitive.Root delayDuration={icon ? 0 : 200}>
+        <S.StyledTrigger>{children}</S.StyledTrigger>
+        <S.StyledContent
+          sideOffset={icon ? 8 : 14}
+          side={icon ? 'right' : 'top'}
+          align="center"
+          {...props}
+        >
+          {content}
+          <TooltipPrimitive.Arrow width={18} height={8} />
+        </S.StyledContent>
+      </TooltipPrimitive.Root>
+    </TooltipPrimitive.Provider>
   )
 }
